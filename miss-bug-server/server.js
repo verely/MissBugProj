0f95c77655a0905a18980386bfa8e240ae085ac9
Valery <verelly.v@gmail.com>
2024-05-02 19:49:43 +0300

    CORS origin fixed

--- miss-bug-server/server.js
+++ miss-bug-server/server.js
@@ -10,6 +10,7 @@ const corsOptions = {
     origin: [
         'http://127.0.0.1:5173',
         'http://127.0.0.1:3000',
+        'http://127.0.0.1:3030',
         'http://localhost:5173',
         'http://localhost:3000',
         'http://localhost:3030'
