f697d2366d057709c540ed3bc8fb83cd0f826ef9
Valery <verelly.v@gmail.com>
2024-05-02 20:29:43 +0300

    fix render

--- miss-bug-front/src/services/bug.service.js
+++ miss-bug-front/src/services/bug.service.js
@@ -9,7 +9,7 @@ import { storageService } from './storage.service.js'
 
 // const BASE_URL = 'http://localhost:3030/api/bug/'
 // const BASE_URL = '//localhost:3030/api/bug/'
-const BASE_URL = '//localhost:3000/api/bug/'
+const BASE_URL = '/api/bug/'
 // const BASE_URL =  process.env.NODE_ENV !== 'development'
 //         ? '/api/'
 //         : '//localhost:3000/api/'
