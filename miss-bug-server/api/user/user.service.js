c12b531564ec126191c134cb849db0c3241d31f6
Valery <verelly.v@gmail.com>
2024-05-09 17:15:39 +0300

    support user CRUDL and user autentication, added middlewares

---
+++ miss-bug-server/api/user/user.service.js
@@ -0,0 +1,120 @@
+import { dbService } from '../../services/db.service.js'
+import { logger } from '../../services/logger.service.js'
+
+import mongodb from 'mongodb'
+const { ObjectId } = mongodb
+
+export const userService = {
+    query,
+    getById,
+    getByUsername,
+    remove,
+    update,
+    add
+}
+
+async function query(filterBy = {}) {
+    const criteria = _buildCriteria(filterBy)
+    try {
+        const collection = await dbService.getCollection('user')
+        var users = await collection.find(criteria).sort({nickname: -1}).toArray()
+        users = users.map(user => {
+            delete user.password
+            user.createdAt = ObjectId(user._id).getTimestamp()
+            return user
+        })
+        return users
+    } catch (err) {
+        logger.error('Cannot find users', err)
+        throw err
+    }
+}
+
+async function getById(userId) {
+    try {
+        const collection = await dbService.getCollection('user')
+        const user = await collection.findOne({ _id: ObjectId(userId) })
+        delete user.password
+        return user
+    } catch (err) {
+        logger.error(`Error while finding user ${userId}`, err)
+        throw err
+    }
+}
+async function getByUsername(username) {
+    try {
+        const collection = await dbService.getCollection('user')
+        const user = await collection.findOne({ username })
+        delete user.password
+        return user
+    } catch (err) {
+        logger.error(`Error while finding user ${username}`, err)
+        throw err
+    }
+}
+
+async function remove(userId) {
+    try {
+        const collection = await dbService.getCollection('user')
+        await collection.deleteOne({ _id: ObjectId(userId) })
+    } catch (err) {
+        logger.error(`Cannot remove user ${userId}`, err)
+        throw err
+    }
+}
+
+async function update(user) {
+    try {
+        const userToSave = {
+            _id: ObjectId(user._id),
+            username: user.username,
+            fullname: user.fullname,
+            score: user.score
+        }
+        const collection = await dbService.getCollection('user')
+        await collection.updateOne({ _id: userToSave._id }, { $set: userToSave })
+        return userToSave
+    } catch (err) {
+        logger.error(`Cannot update user ${user._id}`, err)
+        throw err
+    }
+}
+
+async function add(user) {
+    try {
+        const existUser = await getByUsername(user.username)
+        if (existUser) throw new Error('Username taken')
+
+        const userToAdd = {
+            username: user.username,
+            password: user.password,
+            fullname: user.fullname,
+            score: user.score || 0
+        }
+        const collection = await dbService.getCollection('user')
+        await collection.insertOne(userToAdd)
+        return userToAdd
+    } catch (err) {
+        logger.error('Cannot insert user', err)
+        throw err
+    }
+}
+
+function _buildCriteria(filterBy) {
+    const criteria = {}
+    if (filterBy.txt) {
+        const txtCriteria = { $regex: filterBy.txt, $options: 'i' }
+        criteria.$or = [
+            {
+                username: txtCriteria
+            },
+            {
+                fullname: txtCriteria
+            }
+        ]
+    }
+    if (filterBy.score) {
+        criteria.score = { $gte: filterBy.score }
+    }
+    return criteria
+}
