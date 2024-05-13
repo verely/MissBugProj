3ce729068e08a797b738d51b800495cd500b8fb5
Valery <verelly.v@gmail.com>
2024-05-13 17:16:08 +0300

    signed up issue fixed

--- miss-bug-server/api/user/user.service.js
+++ miss-bug-server/api/user/user.service.js
@@ -34,7 +34,7 @@ async function getById(userId) {
     try {
         const collection = await dbService.getCollection('user')
         const user = await collection.findOne({ _id: new ObjectId(userId) })
-        delete user.password
+        
         return user
     } catch (err) {
         logger.error(`Error while finding user ${userId}`, err)
@@ -45,7 +45,7 @@ async function getByUsername(username) {
     try {
         const collection = await dbService.getCollection('user')
         const user = await collection.findOne({ username })
-        delete user?.password
+
         return user
     } catch (err) {
         logger.error(`Error while finding user ${username}`, err)
